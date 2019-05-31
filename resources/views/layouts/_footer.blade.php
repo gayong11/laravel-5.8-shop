918f803cde9d793319a84fc7b349a1f3db84736e
vagrant <vagrant@homestead>
2019-05-31 07:03:04 +0000

    test

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="javascript: void(0)">gaoyong模仿Laravel社区项目编辑</a>
+        </p>
+
+        <p class="float-right">
+            <a href="gaoyong@126.com">联系我</a>
+        </p>
+    </div>
+</footer>
