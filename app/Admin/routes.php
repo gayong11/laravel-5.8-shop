c6ee50cfdaffda9942666cd3171c87a63f157cf6
vagrant <vagrant@homestead>
2019-06-18 05:51:09 +0000

    管理后台-订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -17,4 +17,8 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.order.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.order.ship');
+
 });
